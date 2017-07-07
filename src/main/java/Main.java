12541d9462b82aeddc85b9d9fea1b7157b9d8a7f
Jocélio Lima <jocelio27@gmail.com>
2017-07-07 09:58:05 -0300

    Updates

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,14 @@
-/**
- * Created by jocelio.lima on 6/16/2017.
- */
+
 public class Main {
 
     public static void main(String[] args) {
-
-//        String input ="333277778";
-//        String input ="11166611144411";
-//        String input ="222 277772";
-          String input ="886207777336683366222 20555666 6642";
-//        String input = "0123456789";
-
-        final String output = new SMS(input).texto();
-        System.out.println(output);
+        //TODO Implement unit tests
+        System.out.println(new SMS("333277778").texto());
+        System.out.println(new SMS("11166611144411").texto());
+        System.out.println(new SMS("222 277772").texto());
+        System.out.println(new SMS("886207777336683366222 20555666 6642").texto());
+        System.out.println(new SMS("0123456789").texto());
+        
     }
+    
 }
